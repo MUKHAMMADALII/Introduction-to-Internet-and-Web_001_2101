6dcba85d0e47589be1804556257a3f826cec48e8
MUKHAMMADALII <83961590+MUKHAMMADALII@users.noreply.github.com>
2021-05-22 23:19:17 +0900

    Update main(1).cpp

--- main(1).cpp
+++ main(1).cpp
@@ -2,13 +2,13 @@ class MainMenu {
 public:
     MainMenu() {
         cout << "\n\n\n\n";
-        cout << "\t\t"; cout << " □ □ □ □ □  □ □ □ □  □ □ □ □ □   □ □ □ □ □ □   □   □ □ □ □ □ \n";
-        cout << "\t\t"; cout << "     □      □            □       □          □  □   □             \n";
-        cout << "\t\t"; cout << "     □      □            □       □          □  □   □             \n";
+        cout << "\t\t"; cout << " □ □ □ □ □  □ □ □ □  □ □ □ □ □   □ □ □ □ □     □   □ □ □ □ □ \n";
+        cout << "\t\t"; cout << "     □      □            □       □         □   □   □             \n";
+        cout << "\t\t"; cout << "     □      □            □       □         □   □   □             \n";
         cout << "\t\t"; cout << "     □      □ □ □ □      □       □        □    □   □ □ □ □ □ \n";
         cout << "\t\t"; cout << "     □      □            □       □ □  □        □           □ \n";
-        cout << "\t\t"; cout << "     □      □            □       □        □    □           □ \n";
-        cout << "\t\t"; cout << "     □      □ □ □ □      □       □          □  □   □ □ □ □ □ \n\n\n\n\n";
+        cout << "\t\t"; cout << "     □      □            □       □       □     □           □ \n";
+        cout << "\t\t"; cout << "     □      □ □ □ □      □       □         □   □   □ □ □ □ □ \n\n\n\n\n";
         cout << "\t\t"; cout << "                게임을 시작하려면 아무키나 누르세요.\n\n\n\n\n\n\n";
         getchar(); // 아무키 입력 기다림
         system("cls"); // 콘솔 창 clear
